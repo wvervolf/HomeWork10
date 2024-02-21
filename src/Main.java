f31e62462484390f7694ded6bde839b1900d8653
Роман Быков <wvervolf@gmail.com>
2024-02-21 10:26:53 +0300

    HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName = "Ivan"; // имя
+        String middleName = "Ivanovich"; // отчество
+        String lastName = "Ivanov"; // фамилия
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+        System.out.println("Задание 2");
+        System.out.println(fullName.toUpperCase());
+        System.out.println("Задание 3");
+        String firstNameTwo = "Семён"; // имя
+        String middleNameTwo = "Семёнович"; // отчество
+        String lastNameTwo = "Иванов"; // фамилия
+        String fullNameTwo = lastNameTwo + " " + firstNameTwo + " " + middleNameTwo;
+        fullNameTwo = fullNameTwo.replace("ё", "е");
+        System.out.println(fullNameTwo);
     }
+
 }
